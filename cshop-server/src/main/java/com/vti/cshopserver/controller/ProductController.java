3460f5bdfcbbc65dec7a94f86b01863eeda8479b
unknown <chiennguyen2198@gmail.com>
2022-09-29 21:06:34 +0700

    add create product and update product form

--- cshop-server/src/main/java/com/vti/cshopserver/controller/ProductController.java
+++ cshop-server/src/main/java/com/vti/cshopserver/controller/ProductController.java
@@ -1,12 +1,11 @@
 package com.vti.cshopserver.controller;
 
 import com.vti.cshopserver.dto.CategoryDTO;
+import com.vti.cshopserver.dto.ProductDTO;
 import com.vti.cshopserver.entity.Category;
-import com.vti.cshopserver.form.CategoryCreateForm;
-import com.vti.cshopserver.form.CategoryFilterForm;
-import com.vti.cshopserver.form.CategoryUpdateForm;
-import com.vti.cshopserver.repository.ICategoryRepository;
-import com.vti.cshopserver.service.ICategoryService;
+import com.vti.cshopserver.entity.Product;
+import com.vti.cshopserver.form.*;
+import com.vti.cshopserver.service.IProductService;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,38 +17,36 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/categories")
-public class CategoryController {
-
+@RequestMapping("/api/v1/products")
+public class ProductController {
     @Autowired
-    private ICategoryService service;
+    IProductService service;
 
     @Autowired
     private ModelMapper mapper;
-
     @GetMapping
-    public Page<CategoryDTO> findAll(Pageable pageable, CategoryFilterForm form){
-        Page<Category> page = service.findAll(pageable, form);
-        List<CategoryDTO> dtos = mapper.map(
+    public Page<ProductDTO> findAll(Pageable pageable, ProductFilterForm form){
+        Page<Product> page = service.findAll(pageable, form);
+        List<ProductDTO> dtos = mapper.map(
                 page.getContent(),
-                new TypeToken<List<CategoryDTO>>() {}.getType()
+                new TypeToken<List<ProductDTO>>() {}.getType()
 
         );
         return new PageImpl<>(dtos,pageable, page.getTotalElements());
     }
 
     @GetMapping("/{id}")
-    public CategoryDTO findById(@PathVariable("id") int id){
-        return mapper.map(service.findById(id), CategoryDTO.class);
+    public ProductDTO findById(@PathVariable("id") int id){
+        return mapper.map(service.findById(id), ProductDTO.class);
     }
 
     @PostMapping
-    public void create(@RequestBody CategoryCreateForm form) {
+    public void create(@RequestBody ProductCreateForm form) {
         service.create(form);
     }
 
     @PutMapping("/{id}")
-    public void update(@PathVariable("id") int id, @RequestBody CategoryUpdateForm form){
+    public void update(@PathVariable("id") int id, @RequestBody ProductUpdateForm form){
         form.setId(id);
         service.update(form);
     }
@@ -60,3 +57,5 @@ public class CategoryController {
     }
 
 }
+
+
---
+++ cshop-server/src/main/java/com/vti/cshopserver/specification/CategorySpecification.java
@@ -0,0 +1,5 @@
+package com.vti.cshopserver.specification;
+
+public class CategorySpecification {
+    
+}
